7da62939d6435df850f53c7f41a8832929103da1
Prakhar Agrawal <prakharagrawal2408@gmail.com>
2022-06-06 13:07:16 +0530

    Add to cart Functionality added for customer

--- src/components/beforeLogin/LoggedIn.js
+++ src/components/beforeLogin/LoggedIn.js
@@ -1,12 +1,12 @@
-// import React from "react";
-// import Customer from "../../customer/home/Customer";
-// import Restaurent from "../../restaurent/home/Restaurent";
+import React from "react";
+import Customer from "../afterLogin/customer/home/Customer";
+// import Restaurent from "../afterLogin/restaurent/home/Restaurent";
 import { useSelector } from "react-redux";
 
 const LoggedIn = () => {
   const customer = useSelector((state) => state.logIn.customer);
-  return customer ? console.log("customer") : console.log("restaurent");
-  // return customer ? <Customer /> : <Restaurent />;
+
+  return customer ? <Customer /> : console.log("restaurent");
 };
 
 export default LoggedIn;
--- src/store/index.js
+++ src/store/index.js
@@ -2,18 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 // import accountModalSlice from "./accountModal-slice";
 import authSlice from "./auth-slice";
 // import productSearchSlice from "./productSearch-slice";
-// import restaurentSlice from "./restaurentData-slice";
-// import cartSlice from "./cart-slice";
-// import filterSlice from "./filterSlice";
+import restaurentSlice from "./restaurentData-slice";
+import cartSlice from "./cart-slice";
+import filterSlice from "./filterSlice";
 
 const store = configureStore({
   reducer: {
-    logIn: authSlice.reducer
+    logIn: authSlice.reducer,
     // productSearch: productSearchSlice.reducer,
     // accountModal: accountModalSlice.reducer,
-    // restaurent: restaurentSlice.reducer,
-    // cart: cartSlice.reducer,
-    // filter: filterSlice.reducer,
+    restaurent: restaurentSlice.reducer,
+    cart: cartSlice.reducer,
+    filter: filterSlice.reducer,
   },
 });
 
